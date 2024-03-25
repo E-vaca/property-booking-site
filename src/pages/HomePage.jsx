a1b3adb9a8232528b4930df4c13b84ae5868bfcc
vaxtale2023 <vacaramirez1@gmail.com>
2024-03-24 22:37:04 -0400

    Use data from the api folder directly. Not calling any apit yet

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,49 +1,55 @@
 import { useState } from 'react';
 
-import { 
-    isListingAvailable,
-    listings as staticListings } from '@/api/data/listings';
+import {
+  isListingAvailable,
+  listings as staticListings,
+} from '@/api/data/listings';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
 import { Separator } from '@/components/ui';
 
 const HomePage = () => {
-    const [listings, setListings] = useState(staticListings)
+  const [listings, setListings] = useState([]);
 
-    const handleFilters = (filters) => {
-        const { dates, guests, search } = filters
+  const handleFilters = (filters) => {
+    const { dates, guests, search } = filters;
 
-        let filteredListings = staticListings
+    // Resets filters by using static listings
+    let filteredListings = staticListings;
 
-        if (dates) {
-            filteredListings = filteredListings.filter((listing) =>
-                isListingAvailable(listing, dates)
-            )
-        }
-
-        if (guests) {
-            filteredListings = filteredListings.filter((listing) => 
-                guests <= listing.maxGuests
-            )
-        }
-
-        if (search) {
-            filteredListings = filteredListings.filter((listing) => 
-                listing.name.toLowerCase().includes(search.toLowerCase()))
-        }
-
-        setListings(filteredListings)
+    // Handles date range
+    if (dates) {
+      filteredListings = filteredListings.filter((listing) =>
+        isListingAvailable(listing, dates),
+      );
     }
 
-    return (
-    <div className='containter py-4'>
-        <div className='mb-4'>
-            <ListingFilters onChange={handleFilters} />
-            <Separator className='my-4' />
-        </div>
-        <ListingList listings = {listings} />
+    // Handles guests
+    if (guests) {
+      filteredListings = filteredListings.filter(
+        (listing) => guests <= listing.maxGuests,
+      );
+    }
+
+    // Handles search
+    if (search) {
+      filteredListings = filteredListings.filter((listing) =>
+        listing.name.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    setListings(filteredListings);
+  };
+
+  return (
+    <div className='container py-4'>
+      <div className='mb-4'>
+        <ListingFilters onChange={handleFilters} />
+        <Separator className='my-4' />
+      </div>
+      <ListingList listings={listings} />
     </div>
-    )
-}
+  );
+};
 
 export default HomePage;
